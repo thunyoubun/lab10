6f41b8a76db9b4bcfea91902734c815d7f28c810
Switch <thunyoubun@gmail.com>
2021-01-12 17:42:25 +0700

    lab10

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,31 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+
+int n;
+void rollGrade(){
+    n = rand()%9;
+    
+}
+
+int main(){
+    string grade[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F" ,"W"};
+    srand(time(0));
+    rollGrade();
+    cout << "Press Enter 3 times to reveal your future.";
+    
+    for(int i= 0; i<3; i++){
+        cin.get();
+
+    }
+    cout << "You will get " << grade[n] << " in this 261102.";
+    
+
+
+
+
+return 0;
+}
